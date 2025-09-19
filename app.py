9488769e04c7a35a42803725260efa0193513ed1
Divya Lingala <dlingala@terpmail.umd.edu>
2025-09-19 16:23:38 -0400

    updated file with render function for about html file

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from jinja2.lexer import newline_re
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Divya Lingala! This is my first HTML page.'
+    return 'Hello World from Divya Lingala! I am adding my first code change.'
 
+@app.route('/hello')
+def hello():  # put application's code here
+    return render_template('hello.html')
+
+@app.route('/about')
+def about():  # put application's code here
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
